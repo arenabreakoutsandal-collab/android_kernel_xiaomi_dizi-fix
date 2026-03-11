06faa81f59c08906c92f2287e6e2edda844e0e78
KSN <redawewrq9@gmail.com>
2026-03-11 17:03:49 +0300

    Add Dizi kernel sources with duplicate symbols and mtdoops fixes

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH="/clang/kernel/linux-x86/clang-r416183b/bin:$PATH"
+
+echo "Cleaning up old build files..."
+rm -rf out/
+
+MAKE_ARGS="O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- CONFIG_SHELL=/bin/bash"
+
+echo "Building a configuration for Dizi (GKI + Vendor)..."
+make $MAKE_ARGS gki_defconfig vendor/dizi_GKI.config
+make $MAKE_ARGS olddefconfigч
+
+CLANG_FLAGS="-Wno-error=frame-larger-than= -Wno-error=shift-count-overflow -Wno-unused-function"
+
+echo "Starting main kernel compilation..."
+make -j$(nproc) $MAKE_ARGS KCFLAGS="$CLANG_FLAGS"
